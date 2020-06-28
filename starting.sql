f52900b72afe9c663d76634e068db9cb170ec850
Fernando Souto <fernandoandradesouto@hotmail.com>
2020-06-28 19:41:54 -0300

    Organização das anotações do Starting.sql

--- starting.sql
+++ starting.sql
@@ -1,31 +1,31 @@
 create database estudo;
 /*
 - Desenvolver uma loja dentro de um jogo, no qual a moeda principal do personagem é chamada de "ZEN"
-- Table usuarios
-	id
-    login
-	senha
-    zen
-- Table loja
-	id
-    item
-    preco
-    qtd
-- Table compras
-	id
-    id_usuario
-    item
-    qtd
+///Table usuarios
+-- id
+-- login
+-- senha
+-- zen
+///Table loja
+-- id
+-- item
+-- preco
+-- qtd
+///Table compras
+-- id
+-- id_usuario
+-- item
+-- qtd
 */
 /*USANDO A TABELA CRIADA*/
 use estudo;
 /*TABELA USUARIOS*/
 create table usuarios(
 	id int unsigned unique not null auto_increment,
-    primary key(id),
-    login varchar(15) not null,
-    senha varchar(20) not null,
-    zen double unsigned not null default 0
+	primary key(id),
+	login varchar(15) not null,
+	senha varchar(20) not null,
+	zen double unsigned not null default 0
 );
 
 insert into usuarios(login,senha) values ('login','senha');
@@ -33,10 +33,10 @@ insert into usuarios(login,senha) values ('login','senha');
 /*TABELA DA LOJA*/
 create table loja(
 	id int unsigned unique not null auto_increment,
-    primary key(id),
-    item varchar(100) not null,
-    preco double unsigned not null,
-    qtd int not null default 0
+	primary key(id),
+	item varchar(100) not null,
+	preco double unsigned not null,
+	qtd int not null default 0
 );
 
 insert into loja(item,preco) values ('lapis',1.50);
@@ -54,11 +54,11 @@ update loja set qtd = 50 where id = 4; /*caderno*/
 /*TABELA DE COMPRAS*/
 create table compras(
 	id int unsigned unique not null auto_increment,
-    primary key(id),
-    id_usuario int unsigned not null,
-    item int unsigned not null,
-    constraint fk_compras_loja foreign key (item) references loja(id),
-    qtd int unsigned not null default 1
+	primary key(id),
+	id_usuario int unsigned not null,
+	item int unsigned not null,
+	constraint fk_compras_loja foreign key (item) references loja(id),
+	qtd int unsigned not null default 1
 );
 
 insert into compras(id_usuario,item,qtd) values(1,2,3);
